fb86ad785c1f951cee4a00a6b7f163564b518693
Alexander Pardo <alexanderpardo@outlook.com>
2022-09-13 01:37:27 -0500

    PrimerSubida

---
+++ Gym.App/Gym.App.Dominio/Entidades/BodyPart.cs
@@ -0,0 +1,11 @@
+using System;
+//Clase enumerada - estas no usan el using system?
+namespace Gym.App.Dominio
+{
+    public enum BodyPart{
+        //Definimos los valores que va a tener este objeto
+        UpperBody,
+        LowerBody,
+        AbdominalCore
+    }
+}
---
+++ Gym.App/Gym.App.Dominio/Entidades/Category.cs
@@ -0,0 +1,14 @@
+using System;
+//Clase enumerada
+namespace Gym.App.Dominio
+{
+    public enum Category{
+        //Definimos los valores que va a tener este objeto
+        Hit,
+        Power,
+        Pilates,
+        Kickboxing,
+        Cardio,
+        Yoga
+    }
+}
